8b03b90fc3e18c3b4a4953cb4a61b63b5f195204
Cesarintapia <tianfangbei@gmail.com>
2021-12-01 17:02:02 -0800

    Shipping

--- BRANCHING SUBMISSION ASSIGNMENT/Program.cs
+++ BRANCHING SUBMISSION ASSIGNMENT/Program.cs
@@ -18,25 +18,30 @@ namespace BRANCHING_SUBMISSION_ASSIGNMENT
             if (weightPackage > 50) //Clausule to weightPackage no more tha 50
             {
                 Console.WriteLine("Sorry!!. Package too heavy to be shipped via Package Express. Have a good day."); // if weightPackage more tha 50, print this message
+                Console.ReadLine();
             }
-            //Section to calculate dimensions and volumen. 
-            Console.WriteLine("\tPlease enter the package width:"); //Ask width
-            int widthPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer  widthPackage
-            Console.WriteLine("\tPlease enter the package height:"); //Ask heigth
-            int heightPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer heightPackage
-            Console.WriteLine("\tPlease enter the package length:"); //Ask length
-            int lengthPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer lengthPackage
-            int dimensions = widthPackage + heightPackage + lengthPackage; //Calculate dimensions
-            int volumen= widthPackage * heightPackage * lengthPackage; //calculate volumen
 
-            if (dimensions > 50) //Clausule to dimensions no more tha 50
+            else
             {
-                Console.WriteLine("Sorry!!. Package too big to be shipped via Package Express. Have a good day."); // if dimensions more tha 50, print this message
+                //Section to calculate dimensions and volumen. 
+                Console.WriteLine("\tPlease enter the package width:"); //Ask width
+                int widthPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer  widthPackage
+                Console.WriteLine("\tPlease enter the package height:"); //Ask heigth
+                int heightPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer heightPackage
+                Console.WriteLine("\tPlease enter the package length:"); //Ask length
+                int lengthPackage = Convert.ToInt32(Console.ReadLine()); //Convert to integer lengthPackage
+                int dimensions = widthPackage + heightPackage + lengthPackage; //Calculate dimensions
+                int volumen = widthPackage * heightPackage * lengthPackage; //calculate volumen
+
+                if (dimensions > 50) //Clausule to dimensions no more tha 50
+                {
+                    Console.WriteLine("Sorry!!. Package too big to be shipped via Package Express. Have a good day."); // if dimensions more tha 50, print this message
+                }
+                double priceShipping = (volumen * weightPackage) / 100; // Opetation for calculate price
+                Console.WriteLine("\tYour estimated total for shipping this package is: " + priceShipping + "\n\tThanks You!!"); //Print total price of shipping
+                Console.ReadLine();
             }
 
-            double priceShipping = (volumen * weightPackage) / 100; // Opetation for calculate price
-            Console.WriteLine("\tYour estimated total for shipping this package is: " + priceShipping + "\n\tThanks You!!"); //Print total price of shipping
-            Console.ReadLine();
         }
     }
 }

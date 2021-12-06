bbb0524664a6370b78f63149f60382eed32974a1
Cesarintapia <tianfangbei@gmail.com>
2021-12-06 12:07:06 -0800

    String and integers

---
+++ Exhand/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exhand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing by two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divide by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please write a whole number");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Divide by zero it is impossible");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine("");
+            }
+        }
+    }
+}
---
+++ Strings and Integers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_and_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] integersList = { 45, 12, 45, 43, 99, 89, 21, 5, 0 };                         //Starting the List
+                Console.WriteLine("Pick a number.");                                               //Ask for a number
+                int numberUser = Convert.ToInt32(Console.ReadLine());                              //Save the response in "numberUser"
+                Console.WriteLine(" When " + numberUser + " Divide the List... the result is:");   //Print message to start
+                for (int i = 0; i < integersList.Length; i++)                                      //Start loop to use all the list
+                {
+                   int numberResult = integersList[i] / numberUser;                                //Perform the division
+                   Console.WriteLine(numberResult);                                                //Print the result
+                }
+            }
+             catch (FormatException error)                                                         //Exception by format, prevent the user to not type strings
+            {
+                Console.WriteLine("Please write a whole number... string is not valid..." + error.Message);  //Print message to prevent about strings
+            }
+            catch (DivideByZeroException error)                                                     //Exception by zero division
+            {
+                Console.WriteLine(" impossible" + error.Message);                                   //Print message to show not divide by zero
+            }
+                catch (Exception error)                                                             //General exception.
+            {
+                Console.WriteLine(error.Message);
+            }
+            finally                                                                                 //Print defaul message
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Esta seccion works");
+            }
+            Console.ReadLine();
+        }
+    }
+}

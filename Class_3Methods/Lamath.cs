cd9a8b33768d50210eb9916cbf897a2218b8c14d
Cesarintapia <tianfangbei@gmail.com>
2021-12-06 21:01:14 -0800

    Class with 3 methods

---
+++ Class_3Methods/Lamath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_3Methods
+{
+    public class Lamath
+    {
+        public int operation1(int lanum)
+        {
+            int leresult = lanum * 735;
+            return leresult;
+        }
+
+        public int operation2(int lanum)
+        {
+            int leresult = lanum + 101010101;
+            return leresult;
+        }
+
+        public int operation3(int lanum)
+        {
+            int leresult = lanum - 1;
+            return leresult;
+        }
+
+    }
+}
---
+++ Class_3Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_3Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write out a number for math operations: ");            // Ask the user pick a number
+            int lenumero = Convert.ToInt32(Console.ReadLine());                       // Save the number in the integer type "leresult1"
+
+            Lamath lamathObject = new Lamath();                                       //Create an instance the la Class Lamath.
+            int leresult1 = lamathObject.operation1(lenumero);                        //Save the number in the integer type "leresult2"
+            Console.WriteLine(leresult1);                                             //Print the result
+
+            int leresult2 = lamathObject.operation2(lenumero);                        // Save the number in the integer type "leresult2"
+            Console.WriteLine(leresult2);                                             //Print the result
+
+            int leresult3 = lamathObject.operation3(lenumero);                        // Save the number in the integer type "leresult2"
+            Console.WriteLine(leresult3);                                             //Print the result
+
+            Console.ReadLine();
+        }
+    }
+}

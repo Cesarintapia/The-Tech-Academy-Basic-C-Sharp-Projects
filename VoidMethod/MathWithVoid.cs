7ec54e67f3469938e53788e4e43f683a4005ce23
Cesarintapia <tianfangbei@gmail.com>
2021-12-07 12:09:28 -0800

    Void method

---
+++ VoidMethod/MathWithVoid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class MathWithVoid
+    {
+        public void MathParrot(int number1, int number11)
+        {
+            int leresult = number1 + 10000;
+            Console.WriteLine(leresult);
+            Console.WriteLine(number11);
+            return;
+        }
+    }
+}
---
+++ VoidMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathWithVoid mathwithVoid = new MathWithVoid();                                   //Instancing the class in "mathwithVoid" object
+
+            Console.WriteLine("Calling the void method passing two numbers");                 //Print message to the King-Loro fans
+            mathwithVoid.MathParrot(19, 213);                                                 //Call the method in the class, passing in two numbers,
+                                                                                               
+
+            Console.WriteLine("Calling the void method specifying the parameters by name.");  //Other message for the King-Loro fans
+            mathwithVoid.MathParrot(number1:200, number11:166);                               //Call the method in the class, specifying the parameters by name.
+
+
+            Console.ReadLine();
+
+        }
+    }
+}

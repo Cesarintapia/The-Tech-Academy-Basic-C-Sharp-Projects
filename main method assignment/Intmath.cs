7c2cb4399d0c8d3fc7686c487d5c691d51b9b135
Cesarintapia <tianfangbei@gmail.com>
2021-12-06 23:04:49 -0800

    Methods with same name

---
+++ main method assignment/Intmath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_method_assignment
+{
+    class Intmath
+    {
+        public int OperaMath(int lenumero)
+        {
+            int paresult = lenumero + 11;
+            return paresult;
+        }
+
+        public int OperaMath(decimal lenumero)
+        {
+            int paresult = Convert.ToInt32(lenumero) * 2;
+            return paresult;
+        }
+
+        public int OperaMath(string lenumero)
+        {
+            int paresult = Convert.ToInt32(lenumero) - 2;
+            return paresult;
+        }
+    }
+}
---
+++ main method assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_method_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intmath operationmathematic = new Intmath();                           //Instance the class "Inmath"
+
+            Console.WriteLine(" Pick one number: ");                               //Ask for number
+            int numeMath1 = Convert.ToInt32(Console.ReadLine());                   //Conver to integer
+            int resultado1 = operationmathematic.OperaMath(numeMath1);             //Make the operation
+            Console.WriteLine(resultado1);
+
+            Console.WriteLine(" Pick one number: ");                                //Ask for number
+            decimal numeMath2 = Convert.ToInt32(Console.ReadLine());                //Convert to decimal for be able to make the operation
+            int resultado2 = operationmathematic.OperaMath(numeMath2);              //Make the operation
+            Console.WriteLine(resultado2);
+
+            Console.WriteLine(" Write out something: ");                            //Ask for string
+            string stringMath = Console.ReadLine();                                 //Save the input in string variable "stringMath"
+            int resultado3 = operationmathematic.OperaMath(stringMath);             //Make the operation when is possible
+            Console.WriteLine(resultado3);
+
+
+            Console.ReadLine();
+        }
+    }
+}

5aec9b0cd2c61757a6c1787263069d76a7761961
Cesarintapia <tianfangbei@gmail.com>
2021-12-09 12:11:22 -0800

    generics

---
+++ Parametros generales/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametros_generales
+{
+    public class Employee<T>                      //Employee class take a generic type parameter
+    {
+        public int Id { get; set; }
+        public List<T> things { get; set; }      //Add a property called “things” and have its data type be a generic list
+
+
+    }
+}
--- Parametros generales/Program.cs
+++ Parametros generales/Program.cs
@@ -10,6 +10,23 @@ namespace Parametros_generales
     {
         static void Main(string[] args)
         {
+            Employee<string> listOfThings1 = new Employee<string>();                               //Instantiate an Employee object with type “string” as its generic parameter
+            listOfThings1.things = new List<string> { "Autumalis", "Oratrix", "Guatemalae" };      //Assign a list of strings as the property value of Things
+
+            Employee<int> listOfThings2 = new Employee<int>();                                     //Instantiate an Employee object with type “int” as its generic parameter 
+            listOfThings2.things = new List<int> { 1, 2, 3 };                                      //Assign a list of integers as the property value of Things.
+
+            foreach (string thing1 in listOfThings1.things)                                        //Print one List
+            {
+                Console.WriteLine(thing1);
+
+            };
+            for (int i=0; i<3; i++)                                                                //Print both Lists matching.
+            {
+                Console.WriteLine(listOfThings1.things[i] + ": " + listOfThings2.things[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }

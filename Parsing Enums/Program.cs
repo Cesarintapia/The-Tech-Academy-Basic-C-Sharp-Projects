ac8709c6702f4c5eef81513280f0661a23344a43
Cesarintapia <tianfangbei@gmail.com>
2021-12-09 15:07:38 -0800

    Parsing enums

---
+++ Parsing Enums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();                                                       //instance the class Week
+            Console.WriteLine("Please enter an actual day of the week in spanish.");      //Ask for a spanish day
+            string dayUser = Console.ReadLine();                                          //Save the input in "dayUser"
+
+            Week.Days days;                                                               //Assign the value to a variable of that enum data type
+            try
+            {
+                if (Enum.TryParse(dayUser, true, out days))                               //Compare the "dayUser" with the "Enum Days" and return boolean result
+                {
+
+                }
+                else
+                {
+                    throw new Exception("...Please enter an actual day of the week.");   //Exception when "dayUser" is false
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);                                         //Message default for the wrong input.
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parsing Enums/Week.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    public class Week
+    {
+        public enum Days                       //make the enum
+        {
+            Lunes,
+            Martes,
+            Miercoles,
+            Jueves,
+            Viernes,
+            Sabado,
+            Domingo
+        }
+    }
+}

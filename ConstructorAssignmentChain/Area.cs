62d710439b130d36cc143c64a5ef21160e6ee5a0
Cesarintapia <tianfangbei@gmail.com>
2021-12-13 21:47:09 -0800

    constrcutor chain

---
+++ ConstructorAssignmentChain/Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignmentChain
+{
+    public class Area
+    {
+        public Area(int x) : this(x, x)                                 //contructor that recieve 1 parametrer and chians to the second constructor
+        {
+
+        }
+
+        public Area(int x, int y)                                      //Contructor that recieve 2 parametrers 
+        {
+            int result = x * y;
+            Console.WriteLine("El area es: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorAssignmentChain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignmentChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x = 4;                                   //Declare constant type integer
+            const int y = 78;                                  //Declare constant type integer
+            var z = 4532;                                      //Declare constant type var
+
+            Area rectangulo = new Area(x, y);                  //Claculate the rectangulo area
+
+            Area cuadrado = new Area(z);                       //Claculate the square area
+        }
+    }
+}
